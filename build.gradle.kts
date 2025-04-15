87638572da4a6f79854ba2b20792cc2f5b444aee
Jesus Zazueta <jjzazuet@gmail.com>
2025-04-15 00:51:41 -0400

    Resync master/develop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("io.vacco.oss.gitflow") version "0.9.8" }
 
 group = "io.vacco.murmux"
-version = "2.2.5"
+version = "2.3.0"
 
 configure<io.vacco.oss.gitflow.GsPluginProfileExtension> {
   addJ8Spec()
